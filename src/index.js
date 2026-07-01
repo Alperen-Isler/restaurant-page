f0214c240ce9ef47da7761e585623a6962f68aaf
Alperen-Isler <alperenislerdev@gmail.com>
2026-07-01 14:48:26 +0200

    Add EventListeners for the nav buttons
    add EventListeners for the nav buttons which delete the page and load the new page.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@ import loadHomePage from "./modules/main.js";
 import loadMenuPage from "./modules/menu.js";
 import loadAboutPage from "./modules/about.js";
 
-loadAboutPage();
+loadHomePage();
+
+const homeButton = document.querySelector("#home-btn");
+const menuButton = document.querySelector("#menu-btn");
+const aboutButton = document.querySelector("#about-btn");
+const content = document.querySelector("#content");
+
+homeButton.addEventListener("click", function(){
+    content.innerHTML = "";
+    loadHomePage();
+});
+
+menuButton.addEventListener("click", function(){
+    content.innerHTML = "";
+    loadMenuPage();
+});
+
+aboutButton.addEventListener("click", function(){
+    content.innerHTML = "";
+    loadAboutPage();
+});
